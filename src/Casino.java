402d6d97c9026e29f164a4acc1e9ed1d3d8ac40c
daviskahn25 <davis_kahn25@milton.edu>
2023-05-26 11:19:43 -0400

    final deck

--- src/Casino.java
+++ src/Casino.java
@@ -1,57 +1,33 @@
 import java.util.ArrayList;
 
 public class Casino {
-    ArrayList<Card>deck;
+    ArrayList<Card> deck;
     int score;
 
     public static void main(String[] args) {
         Casino game = new Casino();
     }
 
-    public Casino(){
-<<<<<<< HEAD
-        for (int x = 0; x<4; x++) {
-            for(int y = 0; y<13;y++){
-                deck.add(new Card());
-            }
-        deck = new ArrayList<>();
-            deck.add(new Card());
+    public Casino() {
 
 
+        createDeck();
+        printDeck();
     }
 
 
-
-
-
-=======
-        createDeck();
-        printDeck();
-}
-
-
-
-public void createDeck(){
+    public void createDeck() {
         deck = new ArrayList<>();
-    for (int x = 0; x<4; x++) {
-        for(int y = 0; y<13;y++){
-            deck.add(new Card(x,y));
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 13; y++) {
+                deck.add(new Card(x, y));
+            }
+        }
+    }
+
+    public void printDeck() {
+        for (int x = 0; x < 52; x++) {
+            deck.get(x).printCardInfo();
         }
     }
 }
-
-public void printDeck(){
-    for (int x = 0; x<52;x++){
-        deck.get(x).printCardInfo();
-    }
-}
->>>>>>> b27d6de (final deck)
-
-
-
-}
-<<<<<<< HEAD
-=======
-
-
->>>>>>> b27d6de (final deck)
